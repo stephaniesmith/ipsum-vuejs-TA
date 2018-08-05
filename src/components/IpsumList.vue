6e3943eeab395c4107533876520df92134ea00f8
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-04 18:42:31 -0700

    creating files

---
+++ src/components/IpsumList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="ipsumList">
+        <h1>Ipsum List</h1>
+        <ul>
+            <li
+                v-for="ipsum in ipsums"
+                v-bind:key="ipsum.title"
+                v-bind:class="{ selected: selected === ipsum }"
+                v-on:click="$emit('selected', ipsum)"
+            >
+                {{ipsum.title}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import ipsums from '../data/ipsums';
+
+    export default {
+        data() {
+            return { ipsums };
+        },
+        props: ['selected']
+    }
+</script>
+
+<style>
+#ipsumList {
+   background-color: black;
+   color:  white;
+}
+</style>
