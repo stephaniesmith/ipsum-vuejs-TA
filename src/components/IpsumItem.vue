48d6e2fb21fc2c6feb79b314087084400b9acfff
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-22 15:40:12 -0700

    created IpsumItem

---
+++ src/components/IpsumItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li
+    :class="{ selected: ipsum === selected }"
+    @click="handleClick"
+  >
+    {{ipsum.title}}
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['ipsum', 'selected', 'onSelect'],
+    methods: {
+      handleClick() {
+        this.onSelect(this.ipsum);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  li {
+    background-color: white;
+    color: black;
+    padding: 1em;
+    border-radius: .75em;
+    margin: .75em;
+  }
+</style>
