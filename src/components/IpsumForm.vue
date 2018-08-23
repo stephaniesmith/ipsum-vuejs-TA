f1b2bfbd4877d772000af8bd1b065c19a0ac426c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-22 18:56:51 -0700

    working on the IpsumViewer and its children

---
+++ src/components/IpsumForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    
+  </form>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped>
+
+</style>
+
+title: "Bob Ross Ipsum",
+    category: "art",
+    author: "Bob Ross",
+    authorUrl: "https://en.wikipedia.org/wiki/Bob_Ross",
+    publishedOn: "2015-11-05",
+    body:
--- src/components/IpsumViewer.vue
+++ src/components/IpsumViewer.vue
@@ -1,46 +1,34 @@
 <template>
 	<div id="ipsumViewer">
 		<article v-if="ipsum">
-			<h2>{{ipsum.title}}</h2>
-			<p id="author">
-				Posted on {{ipsum.publishedOn}} in {{ipsum.category}}
-				by <a :href="ipsum.authorUrl">{{ipsum.author}}</a>
-			</p>
-			<div id="body" v-html="ipsum.body"></div>
+      <IpsumForm/>
+			<IpsumDisplay :ipsum="ipsum"/>
 		</article>
 		<p v-else>Please select an ipsum</p>
 	</div>
 </template>
 
 <script>
-	export default {
-		data: { foo: 'foo' },
-		props: ['ipsum']
-	};
+import IpsumDisplay from './IpsumDisplay.vue';
+import IpsumForm from './IpsumForm.vue';
+
+export default {
+  data: function() {
+    return { 
+      editing: false
+    };
+  },
+  components: {
+    IpsumDisplay,
+    IpsumForm
+  },
+  props: ['ipsum']
+};
 </script>
 
 <style>
   #ipsumViewer {
    	color:  grey;
 		padding: 0 2rem;
-	}
-
-	h2 {
-		font-size: 1.75em;
-		font-weight: bold;
-	}
-
-	#author {
-		font-weight: bold;
-		margin: 1rem 0;
-	}
-
-	#body p {
-		margin-bottom: .75rem;
-	}
-
-	#body img {
-		margin: 1rem 0;
-		width: 100%;
 	}
 </style>
