67762819442686cb80a083d3f195e6307747210e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-23 22:17:00 -0700

    passing props for editing

--- src/components/IpsumViewer.vue
+++ src/components/IpsumViewer.vue
@@ -1,8 +1,15 @@
 <template>
 	<div id="ipsumViewer">
 		<article v-if="ipsum">
-      <IpsumForm/>
-			<IpsumDisplay :ipsum="ipsum"/>
+      <p v-if="!editing">
+        <button @click="editing = true">Edit</button>
+      </p>
+      <IpsumForm v-if="editing"
+        :ipsum="ipsum"
+        :onUpdate="handleUpdate"
+        :onCancel="handleEndEdit"
+      />
+			<IpsumDisplay v-else :ipsum="ipsum"/>
 		</article>
 		<p v-else>Please select an ipsum</p>
 	</div>
@@ -13,6 +20,7 @@ import IpsumDisplay from './IpsumDisplay.vue';
 import IpsumForm from './IpsumForm.vue';
 
 export default {
+  props: ['ipsum', 'onUpdate'],
   data: function() {
     return { 
       editing: false
@@ -22,13 +30,22 @@ export default {
     IpsumDisplay,
     IpsumForm
   },
-  props: ['ipsum']
+  methods: {
+    handleEndEdit() {
+      this.editing = false;
+    },
+    handleUpdate(ipsum) {
+      console.log('here!');
+      this.onUpdate(ipsum);
+      this.handleEndEdit();
+    }
+  }
 };
 </script>
 
 <style>
   #ipsumViewer {
-   	color:  grey;
-		padding: 0 2rem;
+    color:  grey;
+    padding: 0 2rem;
 	}
 </style>
